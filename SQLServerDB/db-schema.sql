fad4c5746f14c257c51f43e4eda6567b2e3b552a
Román PG <roman.perez.g94@gmail.com>
2021-10-26 20:58:37 -0400

    Added tables. Needs to be tested

--- SQLServerDB/db-schema.sql
+++ SQLServerDB/db-schema.sql
@@ -1,1 +1,52 @@
 --Script
+USE master;
+GO
+
+DROP DATABASE IF EXISTS LIBRARYIBT;
+GO
+
+CREATE DATABASE LIBRARYIBT;
+GO
+
+USE LIBRARYIBT;
+GO
+
+CREATE TABLE PersonaType(
+    PersonaTypeId INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+    BookLimit INT NOT NULL,
+    DaysLimit INT NOT NULL
+);
+
+CREATE TABLE Persona(
+    PersonaId INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+    FirstName NVARCHAR(200) NOT NULL,
+    LastName NVARCHAR(200) NOT NULL,
+    FOREIGN KEY (PersonaTypeId) REFERENCES PersonaType(PersonaTypeId)
+);
+
+CREATE TABLE Book(
+    BookId INT NOT NULL IDENTITY(1, 1) PRIMARY KEY
+    Title NVARCHAR(200) NOT NULL,
+    Copies INT NOT NULL,
+    Publisher NVARCHAR(200) NOT NULL
+
+);
+
+CREATE TABLE PersonaBook(
+    PRIMARY KEY (PersonaId) REFERENCES Persona(PersonaId),
+    PRIMARY KEY (BookId) REFERENCES Book(BookId),
+    AcqDate DATETIME NOT NULL DEFAULT(GETDATE()),
+    Fine INT NOT NULL,
+);
+
+CREATE TABLE Author(
+    AuthorId INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+    FirstName NVARCHAR(200) NOT NULL,
+    LastName NVARCHAR(200) NOT NULL
+);
+
+CREATE TABLE BookAuthor(
+    PRIMARY KEY (BookId) REFERENCES Book(BookId),
+    PRIMARY KEY (AuthorId) REFERENCES Author(AuthorId),
+    
+);
